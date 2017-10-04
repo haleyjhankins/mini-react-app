985ccf9c310f9e0f297dd9694c960b307b62e65f
Haley Hankins <hhankins@clearc2.com>
2017-10-04 15:50:14 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/header';
+
 class App extends Component {
   render() {
     return (
@@ -10,7 +12,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">To-Do App</h1>
         </header>
-        
+
+        <div className="todo-wrapper">
+          <Header />
+        </div>
+
       </div>
     );
   }
